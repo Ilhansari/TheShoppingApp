fe5993614c5d577e1bf4564492a81f9f124f6656
Ilhansari <ilhansari94@hotmail.com>
2022-01-11 11:38:19 +0300

    Add `UIKitExtensions` module for extension

---
+++ UIKitExtensions/UIKitExtensions/NSObject+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  NSObject+Extensions.swift
+//  UIKitExtensions
+//
+//  Created by Ilhan Sari on 11.01.2022.
+//
+
+import Foundation
+
+public protocol ViewIdentifier: AnyObject {
+    var viewIdentifier: String { get }
+    static var viewIdentifier: String { get }
+}
+
+public extension ViewIdentifier {
+    static var viewIdentifier: String {
+        String(describing: self)
+    }
+
+    var viewIdentifier: String {
+        let typeOfSelf = type(of: self)
+        return String(describing: typeOfSelf)
+    }
+}
+
+extension NSObject: ViewIdentifier {}
---
+++ UIKitExtensions/UIKitExtensions/UIKitExtensions.h
@@ -0,0 +1,18 @@
+//
+//  UIKitExtensions.h
+//  UIKitExtensions
+//
+//  Created by Ilhan Sari on 11.01.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIKitExtensions.
+FOUNDATION_EXPORT double UIKitExtensionsVersionNumber;
+
+//! Project version string for UIKitExtensions.
+FOUNDATION_EXPORT const unsigned char UIKitExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitExtensions/PublicHeader.h>
+
+
