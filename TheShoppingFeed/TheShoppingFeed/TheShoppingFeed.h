be62efc8c855a3b7f9662cb1d1add23fef30964b
Ilhansari <ilhansari94@hotmail.com>
2022-01-11 09:45:00 +0300

    Create TheShoppingFeed networking module

---
+++ TheShoppingFeed/TheShoppingFeed/TheShoppingFeed.h
@@ -0,0 +1,18 @@
+//
+//  TheShoppingFeed.h
+//  TheShoppingFeed
+//
+//  Created by Ilhan Sari on 11.01.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TheShoppingFeed.
+FOUNDATION_EXPORT double TheShoppingFeedVersionNumber;
+
+//! Project version string for TheShoppingFeed.
+FOUNDATION_EXPORT const unsigned char TheShoppingFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheShoppingFeed/PublicHeader.h>
+
+
