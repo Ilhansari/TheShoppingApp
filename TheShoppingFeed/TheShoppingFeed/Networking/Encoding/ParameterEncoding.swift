971984e029f32ab9c381efb0e36267cd541e6f31
Ilhansari <ilhansari94@hotmail.com>
2022-01-11 09:50:11 +0300

    Implement Encoding file

---
+++ TheShoppingFeed/TheShoppingFeed/Networking/Encoding/ParameterEncoding.swift
@@ -0,0 +1,31 @@
+//
+//  ParameterEncoding.swift
+//  TheShoppingFeed
+//
+//  Created by Ilhan Sari on 11.01.2022.
+//
+
+import Foundation
+
+public typealias Parameters = [String: Any]
+
+public protocol ParameterEncoder {
+    func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws
+}
+
+public enum ParameterEncoding {
+    case urlEncoding
+    public func encode(urlRequest: inout URLRequest,
+                       bodyParameters: Parameters?,
+                       urlParameters: Parameters?) throws {
+        do {
+            switch self {
+            case .urlEncoding:
+                guard let urlParameters = urlParameters else { return }
+                try URLParameterEncoder().encode(urlRequest: &urlRequest, with: urlParameters)
+            }
+        } catch {
+            throw error
+        }
+    }
+}
