7fcec3bafce4ba51f2d58d21e9c3877ac415705a
Ilhansari <ilhansari94@hotmail.com>
2022-01-11 10:55:10 +0300

    Add `TheShoppingApp` project in workspace

---
+++ TheShoppingApp/TheShoppingAppTests/TheShoppingAppTests.swift
@@ -0,0 +1,13 @@
+//
+//  TheShoppingAppTests.swift
+//  TheShoppingAppTests
+//
+//  Created by Ilhan Sari on 11.01.2022.
+//
+
+import XCTest
+@testable import TheShoppingApp
+
+class TheShoppingAppTests: XCTestCase {
+
+}
