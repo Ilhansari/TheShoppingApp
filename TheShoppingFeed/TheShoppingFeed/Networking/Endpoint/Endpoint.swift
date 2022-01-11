63e8ba52481355c772e7ff986c6f0bad01bbb979
Ilhansari <ilhansari94@hotmail.com>
2022-01-11 09:56:31 +0300

    Add Endpoint file

---
+++ TheShoppingFeed/TheShoppingFeed/Networking/Endpoint/Endpoint.swift
@@ -0,0 +1,44 @@
+//
+//  Endpoint.swift
+//  TheShoppingFeed
+//
+//  Created by Ilhan Sari on 11.01.2022.
+//
+
+import Foundation
+
+public enum ProductAPI {
+    case searchProduct(query: String, page: Int)
+}
+
+extension ProductAPI: NetworkProtocol {
+
+    public var baseURL: URL {
+        return URL(string: "https://bdk0sta2n0.execute-api.eu-west-1.amazonaws.com/ios-assignment/")!
+    }
+
+    public var path: String {
+        switch self {
+        case .searchProduct:
+            return "search"
+        }
+    }
+
+    public var httpMethod: HTTPMethod {
+        return .get
+    }
+
+    public var task: HTTPTask {
+        switch self {
+        case .searchProduct(let query, let page):
+            return .requestParameters(bodyParameters: nil,
+                                      bodyEncoding: .urlEncoding,
+                                      urlParameters: ["query": query,
+                                                      "page": page])
+        }
+    }
+
+    public var headers: HTTPHeaders? {
+        return nil
+    }
+}
